d4e65f2a292c61d8dc67eb4a7d8a173b1eb1ac8c
petarvlajicS3219 <vlajic.p27@gmail.com>
2021-07-16 08:54:39 +0200

    Deploymend Fix

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db.js');
+const path = require('path');
 
 //Route Files
 const news = require('./routes/news');
@@ -17,6 +18,15 @@ app.use(express.json());
 
 //Mount routes
 app.use('/news', news);
+
+// Server static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
